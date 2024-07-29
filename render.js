2b0f26088795501c860026629b3e55d1586cccd0
AskBid <noreply>
2024-07-29 16:52:58 +0100

    first counter in place

--- render.js
+++ render.js
@@ -1,12 +1,45 @@
 const CONJOB_INTERVAL  = 100;
-const SPEED_OF_REFRESH = 100;
+const SPEED_OF_REFRESH = 100; // milliseconds
+var SWITCH = false;
 
-for (let index = 0; index < ((end-start)/CRONJOB_INTERVAL); index++) {
-	await render(start + (CRONJOB_INTERVAL*index), start)
-	// problem! the OVB limit orders get accumulated over time.
-	await sleep(SPEED_OF_REFRESH)
+class Session {
+	constructor (
+		current	
+	) {
+		this.current = {
+			'1' : false, 
+			'1+': false,
+			'2' : false,
+			'2+': false,
+			'3' : false,
+			'3+': false,
+			'4' : false,
+			'4+': false,
+		}
+	}
 }
 
+async function counting(ms) {
+	var counter = 0
+	while (SWITCH) {
+		// await render(start + (CRONJOB_INTERVAL*index), start)
+		// // problem! the OVB limit orders get accumulated over time.
+		counter = counter + 1
+		console.log(counter)
+		await sleep(ms)
+	}
+}
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
+const button = document.getElementById('start_stop')
+button.setAttribute('onclick', "start_stop()")
 
+function start_stop() {
+	const inputBeatms = document.getElementById("beatms");
+	SWITCH = true;
+	counting(inputBeatms.value);
+	button.innerText = "STOP."
+}
